dd776f8e9aac2d1c3deb5f64f1a26e352577854e
Hotch0colate <72353329+Hotch0colate@users.noreply.github.com>
2022-08-22 17:11:57 +0700

    rename file

--- 64090500441.cpp
+++ 64090500441.cpp
@@ -6,10 +6,17 @@ int main()
     int birthday = 5;
     int last_student_number = 1;
     int the_second_one_form_last = 4;
-    int map[11][11] = {};
+
+
+
+    int map[10][10] = {};
     map[rand() % 9][rand() % 9] = 1;
     map[birthday][last_student_number] = 2;
     map[10-birthday][the_second_one_form_last] = 3;
+
+
+
+
     for(int i = 9; i >= 0;i--)
     {
         for(int j = 0 ; j < 10 ; j++){ 
@@ -17,6 +24,11 @@ int main()
         }
         cout << '\n';
     }
+
+
+
+
+
 cout <<"Euclidean distance " << sqrt(pow(birthday - (10-birthday),2) + pow(last_student_number - the_second_one_form_last,2)) << endl;
 cout <<"Manhattan distance " << abs(birthday - (10-birthday)) + abs(last_student_number - the_second_one_form_last) <<  endl;
 cout <<"Chebychev distance " << max(abs(birthday - (10-birthday)),abs(last_student_number - the_second_one_form_last));
